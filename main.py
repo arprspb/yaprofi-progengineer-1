95fae2b07e06977d67e5f7127f26130d5caaf4c3
Artem Prokhorov <arprspb@gmail.com>
2024-03-24 13:27:04 +0300

    random choice range edited

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ difference = set(range(1, N+1)) - set(values)
 if len(difference) == 1:
     print(list(difference)[0])
 else:
-    print(choice(list(values)))
+    print(choice(list(range(1, N+1))))
