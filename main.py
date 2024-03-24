9d5e466657f596304ed5d65ca1822bd5f77442d7
Artem Prokhorov <arprspb@gmail.com>
2024-03-24 13:08:07 +0300

    partial solution

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from random import choice
+
+programmer_name = input()
+
+class EmptryStringException(Exception):
+    pass
+
+data = dict()
+while True:
+    
+    try:
+        inp = input()
+        if inp == '':
+            raise EmptryStringException
+        
+        name, number = inp.split('-')
+        data.update({name:number})
+    except (EmptryStringException, EOFError):
+        break
+    except:
+        break
+
+values = data.values()
+
+N = len(values)+1
+
+difference = set(range(1, N+1)) - set(values)
+if len(difference) == 1:
+    print(list(difference)[0])
+else:
+    print(choice(list(values)))
